cea228e0e1f7773715e4f1a1b17605cf75899c7b
aayushroboto <aayush@robotostudio.com>
2025-08-06 10:27:56 +0530

    style(cta): enhance design with background images, update text colors, and improve layout for better visibility

--- apps/web/src/components/sections/cta.tsx
+++ apps/web/src/components/sections/cta.tsx
@@ -24,28 +24,24 @@ export function CTABlock(props: CTABlockProps) {
 
   return (
     <section id="features" className="my-6 md:my-16">
-      <div className="container mx-auto px-4 md:px-8">
-        <div className="bg-muted py-16 rounded-3xl px-4">
+      <div className="container relative max-w-5xl mx-auto px-4 md:px-8">
+        <div className="bg-[url('/cta-bg.png')] bg-cover bg-center py-16 rounded-2xl px-4 relative z-10">
           <div className="text-center max-w-3xl mx-auto space-y-8">
             {eyebrow && (
-              <Badge
-                variant="secondary"
-                className="bg-zinc-200 dark:text-black"
-                {...inspectorProps({ fieldId: "eyebrow" })}
-              >
+              <Badge {...inspectorProps({ fieldId: "eyebrow" })}>
                 {eyebrow}
               </Badge>
             )}
             <h2
-              className="text-3xl font-semibold md:text-5xl text-balance"
+              className="text-3xl font-semibold md:text-5xl tracking-[-0.96px] text-balance text-white"
               {...inspectorProps({ fieldId: "title" })}
             >
               {title}
             </h2>
-            <div className="text-lg text-muted-foreground">
+            <div className="text-lg text-white">
               <ContentfulRichText
                 richText={richText}
-                className="text-balance"
+                className="text-balance text-white [&_a]:text-white [&_a]:underline [&_a]:underline-offset-4 [&_a]:decoration-white/50"
                 {...inspectorProps({ fieldId: "richText" })}
               />
             </div>
@@ -59,6 +55,8 @@ export function CTABlock(props: CTABlockProps) {
             </div>
           </div>
         </div>
+        <div className="absolute top-10 -left-10 w-48 h-80 opacity-75 bg-gradient-to-b from-zinc-300 via-purple-400 to-rose-300 dark:from-orange-800 dark:via-red-600 dark:to-slate-600 rounded-full blur-[100px] z-0" />
+        <div className="absolute bottom-0 -right-10 w-48 h-80 opacity-75 bg-gradient-to-b from-stone-300 to-emerald-200 dark:from-[#64a51ad6] dark:to-[#689516] rounded-full blur-[100px]" />
       </div>
     </section>
   );
